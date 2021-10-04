4a018c8c37541711c5ba9430b6c344b7e61dec5b
Justin Bakse <justin@tsfim.com>
2021-10-04 15:23:56 -0500

    add carousel with glider.js

--- components/slides_builder.js
+++ components/slides_builder.js
@@ -25,62 +25,44 @@ function slidesBuilder(content) {
     }
 
     slides += util.trimLines(`
-          <div class="carousel-item ${i == 0 ? "active" : ""}">
-            <div class="slide">
+            <div class="slide  ${i == 0 ? "active" : ""}">
               <div class="slide-image">
-                <img class="d-block" src="${slide.image}" alt="${slide.title}">
+                <img class="${slide.fit}" src="${slide.image}" alt="${
+      slide.title
+    }">
               </div>
               <div class="slide-info">
-                <div class="slide-info-middle">
-                  
-                  <div class="artist">${slide.artist}</div>
-                  <div class="title">${slide.title}</div>
-                  <div class="comments">${slide.comments}</div>
-
-                </div>
-                <div class="slide-info-bottom">
-                  ${links}
-                </div>
+                <div class="artist">${slide.artist}</div>
+                <div class="title">${slide.title}</div>
+                <div class="comments">${slide.comments}</div>
+                <div class="links">${links}</div>
               </div>
             </div>
-          </div>`);
+          `);
   });
 
   content = util.trimLines(`
-        <div class="slides-wrap">
-      <div id="carousel-${slidesCounter}" class="carousel slide" data-ride="carousel">
-
-        <div class="carousel-inner slides" role="listbox">
-              ${slides}
-        </div>
-
-        <div class="slide-info-top">
-          <a class="carousel-control-prev" href="#carousel-${slidesCounter}" role="button" data-slide="prev">
-            <span class="carousel-prev-icon">
-                <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                  <defs></defs>
-                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <polygon id="Page-1-Copy" fill="#3B7CF4" transform="translate(7.500000, 8.659834) rotate(-180.000000) translate(-7.500000, -8.659834) " points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
-                  </g>
-                </svg>
-            </span>
-            <span class="sr-only">Previous</span>
-          </a>
-
-          <a class="carousel-control-next" href="#carousel-${slidesCounter}" role="button" data-slide="next">
-
-            <span class="carousel-next-icon-tc">
-              <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <defs></defs>
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <polygon fill="#3B7CF4" points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
-                </g>
-              </svg>
-            </span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
+      <div class="slides-component glider-contain">
+        <div class="slides glider">${slides}</div>
+        <nav>
+          <div class="prev">
+            <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <defs></defs>
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <polygon id="Page-1-Copy" fill="#3B7CF4" transform="translate(7.500000, 8.659834) rotate(-180.000000) translate(-7.500000, -8.659834) " points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
+              </g>
+            </svg>
+          </div>
+          <div class="dots"></div>
+          <div class="next">
+            <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <defs></defs>
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <polygon fill="#3B7CF4" points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
+              </g>
+            </svg>
+          </div>
+        </nav>
       </div>
       `);
 
@@ -88,3 +70,31 @@ function slidesBuilder(content) {
 }
 
 module.exports = slidesBuilder;
+
+// <div class="slide-info-top">
+//           <a class="carousel-control-prev" href="#carousel-${slidesCounter}" role="button" data-slide="prev">
+//             <span class="carousel-prev-icon">
+//                 <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//                   <defs></defs>
+//                   <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+//                       <polygon id="Page-1-Copy" fill="#3B7CF4" transform="translate(7.500000, 8.659834) rotate(-180.000000) translate(-7.500000, -8.659834) " points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
+//                   </g>
+//                 </svg>
+//             </span>
+//             <span class="sr-only">Previous</span>
+//           </a>
+
+//           <a class="carousel-control-next" href="#carousel-${slidesCounter}" role="button" data-slide="next">
+
+//             <span class="carousel-next-icon-tc">
+//               <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//                 <defs></defs>
+//                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+//                     <polygon fill="#3B7CF4" points="0 17.3196679 15 8.65334717 0 5.68434189e-14"></polygon>
+//                 </g>
+//               </svg>
+//             </span>
+//             <span class="sr-only">Next</span>
+//           </a>
+//         </div>
+//       </div>
--- js/main.js
+++ js/main.js
@@ -11,11 +11,25 @@ function ready(cb) {
 ready(main);
 
 function main() {
+  // spoilers
   var spoilers = document.getElementsByClassName("spoiler");
   console.log(spoilers);
   for (const spoiler of spoilers) {
     spoiler.addEventListener("click", () => spoiler.classList.add("reveal"));
   }
+
+  // glider slides
+  new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1,
+    dots: ".dots",
+    draggable: true,
+    scrollLock: true,
+    dragVelocity: 1,
+    arrows: {
+      prev: ".prev",
+      next: ".next",
+    },
+  });
 }
 
 // $(main);
