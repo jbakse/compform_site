9fd1b99b6ce19ba36bcf85b5081dd62fcfb3e109
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2024-10-25 00:07:31 -0400

    update procedures mess

--- content/procedures/dots_mess.js
+++ content/procedures/dots_mess.js
@@ -11,11 +11,16 @@ const yMax = 50;
 function setup() {
   const p5_canvas = createCanvas(windowWidth, windowHeight);
   mess(p5_canvas);
-  dots.push({ x: random(xMin, width - xMax), y: random(yMin, height - yMax) });
+  dots.push({
+    x: random(width / 2 - 100, width / 2 + 100),
+    y: random(height / 2 - 100, height / 2 + 100),
+    clicked: false,
+  });
   textAlign(CENTER);
 }
 
 function draw() {
+  clear();
   if (points.length > 1) {
     stroke("#004cff");
     strokeWeight(2);
@@ -25,18 +30,32 @@ function draw() {
   }
 
   for (let i = 0; i < dots.length; i++) {
-    fill("#004cff");
+    let dotSize;
+    let dotColor;
+
+    if (!dots[i].clicked && dist(mouseX, mouseY, dots[i].x, dots[i].y) > 10) {
+      dotSize = 10 + sin(millis() * 0.01) * 2;
+      dotColor = "#ff0000";
+    } else {
+      dotColor = "#004cff";
+      dotSize = 10;
+    }
+    if (dist(mouseX, mouseY, dots[i].x, dots[i].y) < 10){
+      cursor(HAND);
+    }
+    fill(dotColor);
     textSize(14);
     noStroke();
     text(i + 1, dots[i].x, dots[i].y - 10);
-    ellipse(dots[i].x, dots[i].y, 10, 10);
+    ellipse(dots[i].x, dots[i].y, dotSize, dotSize);
   }
 }
 
 function mousePressed() {
   const dot = dots[dots.length - 1];
   if (dist(mouseX, mouseY, dot.x, dot.y) < 10) {
+    dot.clicked = true;
     points.push({ x: dot.x, y: dot.y });
-    dots.push({ x: random(xMin, width - xMax), y: random(yMin, height - yMax) });
+    dots.push({ x: random(xMin, width - xMax), y: random(yMin, height - yMax), clicked: false });
   }
 }
